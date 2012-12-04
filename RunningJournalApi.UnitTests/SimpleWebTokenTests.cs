fbf65bc1999b420fcb575ab71d5ef42e2311db52
Mark Seemann <mark@seemann.ms>
2012-12-04 11:23:39 +0100

    Introduced SimpleWebToken,
    as a sequence of claims.

---
+++ RunningJournalApi.UnitTests/SimpleWebTokenTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Ploeh.Samples.RunningJournalApi;
+using System.Security.Claims;
+
+namespace Ploeh.Samples.RunningJournalApi.UnitTests
+{
+    public class SimpleWebTokenTests
+    {
+        [Fact]
+        public void SutIsIteratorOfClaims()
+        {
+            var sut = new SimpleWebToken();
+            Assert.IsAssignableFrom<IEnumerable<Claim>>(sut);
+        }
+
+        [Fact]
+        public void SutYieldsInjectedClaims()
+        {
+            var expected = new[]
+            {
+                new Claim("foo", "bar"),
+                new Claim("baz", "qux"),
+                new Claim("quux", "corge")
+            };
+            var sut = new SimpleWebToken(expected);
+            Assert.True(expected.SequenceEqual(sut));
+            Assert.True(
+                expected.Cast<object>().SequenceEqual(
+                    ((System.Collections.IEnumerable)sut).OfType<object>()));
+        }
+    }
+}
---
+++ RunningJournalApi/SimpleWebToken.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace Ploeh.Samples.RunningJournalApi
+{
+    public class SimpleWebToken : IEnumerable<Claim>
+    {
+        private readonly IEnumerable<Claim> claims;
+
+        public SimpleWebToken(params Claim[] claims)
+        {
+            this.claims = claims;
+        }
+
+        public IEnumerator<Claim> GetEnumerator()
+        {
+            return this.claims.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
