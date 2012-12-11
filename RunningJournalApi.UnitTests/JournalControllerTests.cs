0a6bd274030d1525ed1ea227561ced1d41373bf3
Mark Seemann <mark@seemann.ms>
2012-12-11 13:58:35 +0100

    Added a Characterization Test for Get action.

---
+++ RunningJournalApi.UnitTests/JournalControllerTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Hosting;
+using Moq;
+using Xunit;
+
+namespace Ploeh.Samples.RunningJournalApi.UnitTests
+{
+    public class JournalControllerTests
+    {
+        [Fact]
+        public void GetReturnsCorrectResult()
+        {
+            // Fixture setup
+            var queryStub = new Mock<IJournalEntriesQuery>();
+            var cmdDummy = new Mock<IAddJournalEntryCommand>();
+            var sut = new JournalController(queryStub.Object, cmdDummy.Object)
+            {
+                Request = new HttpRequestMessage()
+            };
+            sut.Request.Properties.Add(
+                HttpPropertyKeys.HttpConfigurationKey,
+                new HttpConfiguration());
+            sut.Request.Headers.Authorization = 
+                new AuthenticationHeaderValue(
+                    "Bearer",
+                    new SimpleWebToken(new Claim("userName", "foo")).ToString());
+
+            var expected = new[]
+                 {
+                     new JournalEntryModel
+                     {
+                         Time = new DateTime(2012, 12, 10),
+                         Distance = 5780,
+                         Duration = TimeSpan.FromMinutes(33)
+                     },
+                     new JournalEntryModel
+                     {
+                         Time = new DateTime(2012, 12, 5),
+                         Distance = 5760,
+                         Duration = TimeSpan.FromMinutes(31)
+                     },
+                     new JournalEntryModel
+                     {
+                         Time = new DateTime(2012, 12, 3),
+                         Distance = 5780,
+                         Duration = TimeSpan.FromMinutes(31)
+                     }
+                 };
+            queryStub.Setup(q => q.GetJournalEntries("foo")).Returns(expected);
+            // Exercise system
+            var response = sut.Get();
+            var actual = response.Content.ReadAsAsync<JournalModel>().Result;
+            // Verify outcome
+            Assert.True(expected.SequenceEqual(actual.Entries));
+            // Teardown
+        }
+    }
+}
