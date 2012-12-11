2c47404ee86a6d280248f0924d087cd876317748
Mark Seemann <mark@seemann.ms>
2012-12-11 15:46:59 +0100

    Added a Characterization Test of GetUserName.

---
+++ RunningJournalApi.UnitTests/SimpleWebTokenUserNameProjectionTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Ploeh.Samples.RunningJournalApi.UnitTests
+{
+    public class SimpleWebTokenUserNameProjectionTests
+    {
+        [Fact]
+        public void GetUserNameFromProperSimpleWebTokenReturnsCorrectResult()
+        {
+            // Fixture setup
+            var sut = new SimpleWebTokenUserNameProjection();
+
+            var request = new HttpRequestMessage();
+            request.Headers.Authorization = 
+                new AuthenticationHeaderValue(
+                    "Bearer",
+                    new SimpleWebToken(new Claim("userName", "foo")).ToString());
+            // Exercise system
+            var actual = sut.GetUserName(request);
+            // Verify outcome
+            Assert.Equal("foo", actual);
+            // Teardown
+        }
+    }
+}
