dbbe0783154fc499ed01a00f28b28cfcea8afa99
Mark Seemann <mark@seemann.ms>
2012-12-11 21:43:42 +0100

    Invoked Bootstrap from Application_Start.

---
+++ RunningJournal.WebHost/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ploeh.Samples.RunningJournal.WebHost.Global" Language="C#" %>
---
+++ RunningJournal.WebHost/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Security;
+using System.Web.SessionState;
+using Ploeh.Samples.RunningJournalApi;
+
+namespace Ploeh.Samples.RunningJournal.WebHost
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            new Bootstrap().Configure(GlobalConfiguration.Configuration);
+        }
+    }
+}
