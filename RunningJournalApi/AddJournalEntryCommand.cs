ccb7534dbca4112b63d20adab17bbbc3dc46b74a
Mark Seemann <mark@seemann.ms>
2012-12-11 13:29:41 +0100

    Made private class public.

---
+++ RunningJournalApi/AddJournalEntryCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Samples.RunningJournalApi
+{
+    public class AddJournalEntryCommand : IAddJournalEntryCommand
+    {
+        private readonly dynamic db;
+
+        public AddJournalEntryCommand(dynamic db)
+        {
+            this.db = db;
+        }
+
+        public void AddJournalEntry(JournalEntryModel journalEntry, string userName)
+        {
+            var userId = this.db.User
+                .FindAllByUserName(userName)
+                .Select(this.db.User.UserId)
+                .ToScalarOrDefault<int>();
+            if (userId == 0)
+                userId = this.db.User.Insert(UserName: userName).UserId;
+
+            this.db.JournalEntry.Insert(
+                UserId: userId,
+                Time: journalEntry.Time,
+                Distance: journalEntry.Distance,
+                Duration: journalEntry.Duration);
+        }
+    }
+}
--- RunningJournalApi/JournalController.cs
+++ RunningJournalApi/JournalController.cs
@@ -45,32 +45,6 @@ namespace Ploeh.Samples.RunningJournalApi
             return this.Request.CreateResponse();
         }
 
-        private class AddJournalEntryCommand : IAddJournalEntryCommand
-        {
-            private readonly dynamic db;
-
-            public AddJournalEntryCommand(dynamic db)
-            {
-                this.db = db;
-            }
-
-            public void AddJournalEntry(JournalEntryModel journalEntry, string userName)
-            {
-                var userId = this.db.User
-                    .FindAllByUserName(userName)
-                    .Select(this.db.User.UserId)
-                    .ToScalarOrDefault<int>();
-                if (userId == 0)
-                    userId = this.db.User.Insert(UserName: userName).UserId;
-
-                this.db.JournalEntry.Insert(
-                    UserId: userId,
-                    Time: journalEntry.Time,
-                    Distance: journalEntry.Distance,
-                    Duration: journalEntry.Duration);
-            }
-        }
-
         private string GetUserName()
         {
             SimpleWebToken swt;
